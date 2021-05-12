a0f8fa6526c4e37b708a0290a3d52cb79e9cae5d
Exon <exoncabral1@gmail.com>
2021-05-12 18:59:20 +0200

    save

---
+++ responsive.js
@@ -0,0 +1,13 @@
+function color(){
+    let cuadro1=document.getElementById("div");
+    cuadro1.style.backgroundColor="blue";
+}
+document.addEventListener("keydown", color);
+
+function color2(){
+    let cuadro1=document.getElementById("div");
+    cuadro1.style.backgroundColor="red";
+}
+document.addEventListener("keyup", color2);
+
+
